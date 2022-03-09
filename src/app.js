876d76305eaa1bfe77709de9b297c6b4bdeaa55d
toaibv <buitoai2031@gmail.com>
2022-03-09 19:25:56 +0700

    Day1

---
+++ src/app.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var b = 30;
+var myName = "Bui Viet Toai";
+var status = true;
+var myob = {};
+var numberArr = [1, 3, 5, 6];
+var objectArr = [{ id: 1 }, { id: 2 }];
---
+++ src/app.ts
@@ -0,0 +1,17 @@
+export{}
+const b: number = 30;
+const myName: string = "Bui Viet Toai";
+const status:boolean = true;
+
+const myob: object = {};
+const numberArr:number[]= [1,3,5,6];
+
+const objectArr: user[] = [{id:1, name: 'Toai'}, {id:2, name: 'Hung'}]
+
+// Khai baos bieen
+
+type user = {
+    id:number,
+    name:string
+}
+
