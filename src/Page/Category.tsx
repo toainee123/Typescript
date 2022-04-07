7a9b0efed8109d08bfde44c580d9940e54c91c10
_toaineee <90063832+toainee123@users.noreply.github.com>
2022-04-07 15:14:25 +0700

    feat: api update

--- src/Page/Category.tsx
+++ src/Page/Category.tsx
@@ -25,7 +25,9 @@ const Category = ({category, RemoveCate}: CategoryProps) => {
                         <td>{item.name}</td>
                         <td><button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" 
                         onClick= {() => RemoveCate(item._id)}>remove</button></td>
-                        <td><button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"><Link to={`update/${item._id}`}>Edit</Link></button></td>
+                        <td><button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                            <Link to={`category/${item._id}`}>Edit</Link>
+                        </button></td>
                     </tr>
                 )}
             </tbody>
